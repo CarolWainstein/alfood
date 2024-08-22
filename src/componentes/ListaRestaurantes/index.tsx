7f0d9fc2c6c333246b1242558df424c85f47429c
Carolina Wainstein <142952348+CarolWainstein@users.noreply.github.com>
2024-08-22 22:37:36 +1000

    Add files via upload

---
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -0,0 +1,84 @@
+import axios, { AxiosRequestConfig } from 'axios';
+import { useEffect, useState } from 'react';
+import { IPaginacao } from '../../interfaces/IPaginacao';
+import IRestaurante from '../../interfaces/IRestaurante';
+import style from './ListaRestaurantes.module.scss';
+import Restaurante from './Restaurante';
+import { IParametrosBusca } from '../../interfaces/IParametrosBusca';
+
+const ListaRestaurantes = () => {
+
+  const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
+  const [proximaPagina, setProximaPagina] = useState('')
+  const [paginaAnterior, setPaginaAnterior] = useState('')
+  const [busca, setBusca] = useState('')
+  const [ordenacao, setOrdenacao] = useState('')
+
+  const carregarDados = (url: string, opcoes: AxiosRequestConfig = {}) => {
+    axios.get<IPaginacao<IRestaurante>>(url)
+      .then(resposta => {
+        setRestaurantes(resposta.data.results)
+        setProximaPagina(resposta.data.next)
+        setPaginaAnterior(resposta.data.previous)
+      })
+      .catch(erro => {
+        console.log(erro)
+      })
+  }
+
+  useEffect(() => {
+    // obter restaurantes
+    carregarDados('http://localhost:8000/api/v1/restaurantes/')
+  }, [])
+
+  const buscarRestaurante = (evento: React.FormEvent<HTMLFormElement>) => {
+    evento.preventDefault()
+
+    const opcoes = {
+      params: {} as IParametrosBusca
+    }
+    if (busca) {
+      opcoes.params.search = busca
+    }
+    carregarDados('http://localhost:8000/api/v1/restaurantes/', opcoes)
+  }
+
+  return (<section className={style.ListaRestaurantes}>
+    <h1>Os restaurantes mais <em>bacanas</em>!</h1>
+
+    {/* sinta-se livre para deixar o formulário mais elegante, aplicando estilos CSS */}
+   
+    <form onSubmit={buscarRestaurante}>
+      <div className={style.menu_busca}>
+        <div>
+          <input type="text" value={busca} onChange={evento => setBusca(evento.target.value)} />
+        </div>
+        <div className={style.menu_busca_ordenacao}>
+          <label htmlFor="select-ordenacao">Ordenação</label>
+          <select
+            name="select-ordenacao"
+            id="select-ordenacao"
+            value={ordenacao}
+            onChange={evento => setOrdenacao(evento.target.value)}
+          >
+            <option value="">Padrão</option>
+            <option value="id">Por ID</option>
+            <option value="nome">Por Nome</option>
+          </select>
+        </div>
+        <div>
+          <button type='submit'>buscar</button>
+        </div>
+      </div>
+    </form>
+    {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
+    {<button onClick={() => carregarDados(paginaAnterior)} disabled={!paginaAnterior}>
+      Página Anterior
+    </button>}
+    {<button onClick={() => carregarDados(proximaPagina)} disabled={!proximaPagina}>
+      Próxima página
+    </button>}
+  </section>)
+}
+
+export default ListaRestaurantes
